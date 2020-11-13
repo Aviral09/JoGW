917b9fbe0b6177903a506e5cb0c66b1b0132a0aa
unknown <rockdev1234560@gmail.com>
2020-11-13 20:39:16 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@
 import "./App.css";
 import Login from './components/Login/Login'
 import Logout from './components/Logout/Logout'
+import Navbar from './components/navbar/navbar';
 
-function App() {
+/*function App() {
 	return (
 		<div className='App'>
 			<Login />
 			<Logout />
+
+		</div>
+	);
+}*/
+
+function App() {
+	return (
+		<div className='App'>
+       <Navbar/>
 		</div>
 	);
 }
