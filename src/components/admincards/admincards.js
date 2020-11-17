8dac6006f8476a49bf2b0e5af507037a412b8a9e
unknown <rockdev1234560@gmail.com>
2020-11-17 16:56:22 +0530

    admin dashboard

--- src/components/admincards/admincards.js
+++ src/components/admincards/admincards.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import React ,{useState} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Container, IconButton } from '@material-ui/core';
 import CheckCircleOutlineIcon from '@material-ui/icons/CheckCircleOutline';
 import Grid from '@material-ui/core/Grid'; 
 import Typography from '@material-ui/core/Typography';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+import Checkbox from '@material-ui/core/Checkbox';
+import TextField from '@material-ui/core/TextField';
 
 const useStyles = makeStyles((theme) => ({
 
@@ -27,14 +27,83 @@ const useStyles = makeStyles((theme) => ({
   jin:{
     marginTop:'8px' 
   }
+
   }));
 
 export default function AdminCards(){
     const classes = useStyles();
+    const [checked, setChecked] = useState(false);
+    const [checked1, setChecked1] = useState(false);
+    const [n,setn] =useState(' ');
+    const list =['Whether article spirits new her covered hastily sitting her. Money witty books nor son add. Chicken age had evening believe but proceed pretend mrs. At missed advice my it no sister. Miss told ham dull knew see she spot near can. Spirit her entire her called. ', 'Whether article spirits new her covered hastily sitting her. Money witty books nor son add. Chicken age had evening believe but proceed pretend mrs. At missed advice my it no sister. Miss told ham dull knew see she spot near can. Spirit her entire her called. ',  'Whether article spirits new her covered hastily sitting her. Money witty books nor son add. Chicken age had evening believe but proceed pretend mrs. At missed advice my it no sister. Miss told ham dull knew see she spot near can. Spirit her entire her called. ', 'Whether article spirits new her covered hastily sitting her. Money witty books nor son add. Chicken age had evening believe but proceed pretend mrs. At missed advice my it no sister. Miss told ham dull knew see she spot near can. Spirit her entire her called. ', 'Whether article spirits new her covered hastily sitting her. Money witty books nor son add. Chicken age had evening believe but proceed pretend mrs. At missed advice my it no sister. Miss told ham dull knew see she spot near can. Spirit her entire her called. ', 'Whether article spirits new her covered hastily sitting her. Money witty books nor son add. Chicken age had evening believe but proceed pretend mrs. At missed advice my it no sister. Miss told ham dull knew see she spot near can. Spirit her entire her called. ','Whether article spirits new her covered hastily sitting her. Money witty books nor son add','Whether article spirits new her covered hastily sitting her. Money witty books nor son add','Whether article spirits new her covered hastily sitting her. Money witty books nor son add','Whether article spirits new her covered hastily sitting her. Money witty books nor son add','Whether article spirits new her covered hastily sitting her. Money witty books nor son add','Whether article spirits new her covered hastily sitting her. Money witty books nor son add','Whether article spirits new her covered hastily sitting her. Money witty books nor son add','Whether article spirits new her covered hastily sitting her. Money witty books nor son add','Whether article spirits new her covered hastily sitting her. Money witty books nor son add','Whether article spirits new her covered hastily sitting her. Money witty books nor son add','Whether article spirits new her covered hastily sitting her. Money witty books nor son add','Whether article spirits new her covered hastily sitting her. Money witty books nor son add','Whether article spirits new her covered hastily sitting her. Money witty books nor son add','Whether article spirits new her covered hastily sitting her. Money witty books nor son add','Whether article spirits new her covered hastily sitting her. Money witty books nor son add','Whether article spirits new her covered hastily sitting her. Money witty books nor son add','Whether article spirits new her covered hastily sitting her. Money witty books nor son add','Whether article spirits new her covered hastily sitting her.',' Money witty books nor son add',' Money witty books nor son add',' Money witty books nor son add',' Money witty books nor son add',' Money witty books nor son add',' Money witty books nor son add',' Money witty books nor son add',' Money witty books nor son add',' Money witty books nor son add',' Money witty books nor son add',' Money witty books nor son add',' Money witty books nor son add',' Money witty books nor son add',' Money witty books nor son add',' Money witty books nor son add',' Money witty books nor son add',' Money witty books nor son add',' Money witty books nor son add',' Money witty books nor son add',' Money witty books nor son add',' Money witty books nor son add']
+
+
+    const handleChange10 = (event) => {
+      setChecked(event.target.checked);
+      if(event.target.checked==true){
+        setn(10);
+      }
+     else setn(0); 
+    };
+
+    const handleChange20 = (event) => {
+      setChecked1(event.target.checked);
+      if(event.target.checked==true){
+        setn(25);
+      }
+      else setn(0); 
+    };
+
+
+    const [value, setValue] = useState(' ');
+
+    const handleChangeinput = (event) => {
+      setValue(event.target.value);
+      if(value==' '){
+        setn(30);
+      }
+          setn(event.target.value);    
+    };
+
 return(
 <React.Fragment>
 
-      {['Whether article spirits new her covered hastily sitting her. Money witty books nor son add. Chicken age had evening believe but proceed pretend mrs. At missed advice my it no sister. Miss told ham dull knew see she spot near can. Spirit her entire her called. ', 'Whether article spirits new her covered hastily sitting her. Money witty books nor son add. Chicken age had evening believe but proceed pretend mrs. At missed advice my it no sister. Miss told ham dull knew see she spot near can. Spirit her entire her called. ',  'Whether article spirits new her covered hastily sitting her. Money witty books nor son add. Chicken age had evening believe but proceed pretend mrs. At missed advice my it no sister. Miss told ham dull knew see she spot near can. Spirit her entire her called. ', 'Whether article spirits new her covered hastily sitting her. Money witty books nor son add. Chicken age had evening believe but proceed pretend mrs. At missed advice my it no sister. Miss told ham dull knew see she spot near can. Spirit her entire her called. ', 'Whether article spirits new her covered hastily sitting her. Money witty books nor son add. Chicken age had evening believe but proceed pretend mrs. At missed advice my it no sister. Miss told ham dull knew see she spot near can. Spirit her entire her called. ', 'Whether article spirits new her covered hastily sitting her. Money witty books nor son add. Chicken age had evening believe but proceed pretend mrs. At missed advice my it no sister. Miss told ham dull knew see she spot near can. Spirit her entire her called. ','Whether article spirits new her covered hastily sitting her. Money witty books nor son add','Whether article spirits new her covered hastily sitting her. Money witty books nor son add','Whether article spirits new her covered hastily sitting her. Money witty books nor son add','Whether article spirits new her covered hastily sitting her. Money witty books nor son add','Whether article spirits new her covered hastily sitting her. Money witty books nor son add','Whether article spirits new her covered hastily sitting her. Money witty books nor son add','Whether article spirits new her covered hastily sitting her. Money witty books nor son add','Whether article spirits new her covered hastily sitting her. Money witty books nor son add','Whether article spirits new her covered hastily sitting her. Money witty books nor son add','Whether article spirits new her covered hastily sitting her. Money witty books nor son add','Whether article spirits new her covered hastily sitting her. Money witty books nor son add','Whether article spirits new her covered hastily sitting her. Money witty books nor son add','Whether article spirits new her covered hastily sitting her. Money witty books nor son add','Whether article spirits new her covered hastily sitting her. Money witty books nor son add','Whether article spirits new her covered hastily sitting her. Money witty books nor son add','Whether article spirits new her covered hastily sitting her. Money witty books nor son add','Whether article spirits new her covered hastily sitting her. Money witty books nor son add','Whether article spirits new her covered hastily sitting her. Money witty books nor son add'].map((text, index) => (
+<Box display="flex"  flexDirection="row" p={1} m={1} >
+        <Box p={1} flexGrow={1} marginLeft='190px' marginTop='7.5px' > 
+         <b>Select</b>
+
+        </Box>
+        <Box p={1} flexGrow={1}  >  
+         <b>First 10</b>   
+        <Checkbox
+        checked={checked}
+        onChange={handleChange10} 
+        inputProps={{ 'aria-label': 'primary checkbox' }}
+      />
+        </Box>
+        <Box p={1} flexGrow={1} >
+         <b>First 25</b>    
+        <Checkbox
+        checked={checked1}
+        onChange={handleChange20} 
+        color="primary"
+        inputProps={{ 'aria-label': 'secondary checkbox' }}
+      />
+        </Box>
+        <Box p={1} flexGrow={1} marginTop='7.5px' > 
+         <b>First </b>
+        <TextField
+          id="standard-multiline-flexible"
+          multiline
+          rowsMax={4}
+          value={value}
+          onChange={handleChangeinput}
+        />
+ 
+        </Box>
+</Box>       
+
+      {list.slice(0,50).map((text, index) => (
     <Grid container direction={'column'} className={classes.jojo}> 
          <Container className={classes.margi}>
    <Grid container direction={'row'}   >
@@ -49,7 +118,7 @@ return(
 <Grid item xs alignContent='flex-end'>
 
 </Grid>
-<IconButton><CheckCircleOutlineIcon></CheckCircleOutlineIcon></IconButton>
+<IconButton  style={{ backgroundColor: index<n ? "green" : "red" }}><CheckCircleOutlineIcon></CheckCircleOutlineIcon></IconButton>
     </Grid> 
 </Container> 
 </Grid>   
@@ -59,5 +128,3 @@ return(
 </React.Fragment>
 )
 }
-
-
