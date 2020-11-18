a495910e35bb9fb0afcaa706643963105c144309
unknown <rockdev1234560@gmail.com>
2020-11-18 10:35:17 +0530

    add button

--- src/components/personal/personal.js
+++ src/components/personal/personal.js
@@ -1,14 +1,8 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
 import Container from '@material-ui/core/Container';
 import Button from '@material-ui/core/Button';
 import Box from '@material-ui/core/Box';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import { CardContent } from '@material-ui/core';
-import Card from '@material-ui/core/Card';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 import Zoom from '@material-ui/core/Zoom';
 import Fab from '@material-ui/core/Fab';
@@ -18,6 +12,8 @@ import Grid from '@material-ui/core/Grid';
 import IconButton from '@material-ui/core/IconButton';
 import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
 import Typography from '@material-ui/core/Typography';
+import AddCircleIcon from '@material-ui/icons/AddCircle';
+import '../navbar/navbar.css';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -58,6 +54,16 @@ const useStyles = makeStyles((theme) => ({
     margi:{
       border: "3px solid black",
       backgroundColor: ' grey'
+    },
+    imgt: {
+      flex:1
+    },
+    leg:{
+      flex: 13,
+    },
+    ter:{
+      width: 11,
+      height: 11,
     }
 }));
 
@@ -111,10 +117,7 @@ export default function Personal (props)  {
         <h3>Messages</h3>
       </Box>
 
-
       {/*Container to show all messages*/}
-
-
       {det.slice(0,50).map((text, index) => (
     <Grid container direction={'column'} > 
          <Container className={classes.margi} >
@@ -134,16 +137,21 @@ export default function Personal (props)  {
 
     </Grid> 
 </Container> 
-</Grid>   
-              
+</Grid>                
       ))}
+    <Grid container direction={'row'}  >
+<Grid item xs className={classes.leg}></Grid>
+    <Grid item  className={classes.imgt} ><IconButton className={classes.ter}><AddCircleIcon className='tera'/></IconButton></Grid>
 
       {/*Scroll to top FAB*/}
+      <Grid item className={classes.imgt} >
       <ScrollTop {...props}>
         <Fab color="secondary" size="small" aria-label="scroll back to top">
           <KeyboardArrowUpIcon />
         </Fab>
       </ScrollTop>
+      </Grid>
+      </Grid>      
     </React.Fragment>
   );
 }
